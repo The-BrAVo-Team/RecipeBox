9dda24aecb162220d4302fc91c566e05e88810df
Vincent Carlton <vcarlton@nevada.unr.edu>
2024-01-23 12:10:55 -0800

    changed startup.sh

--- startup.sh
+++ startup.sh
@@ -1,6 +1,6 @@
-pip install -r requirements.txt
+virtualenv .env && source .env/bin/activate && pip install -r requirements.txt
 python -m flask db init 
-python -m flask db migrate "Initial-migration"
+python -m flask db migrate -m "Initial-migration"
 python -m flask db upgrade
 cd client
 npm i 
