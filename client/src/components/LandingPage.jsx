a68c0d5e3b86151b87df4fa893722ba8ae4ca2c5
Bethvour Chike <bethvourc@gmail.com>
2024-01-29 21:13:44 -0600

    change

--- client/src/components/LandingPage.jsx
+++ client/src/components/LandingPage.jsx
@@ -1,21 +1,24 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import './LandingPage.css'; // Make sure to create this CSS file
 
-const LandingPage = ({ history }) => {
+const LandingPage = () => {
+  let navigate = useNavigate();
+
   return (
     <div className="landing-container">
       <div className="overlay">
         <header>
           <h1>Grandma's Recipe Box</h1>
           <nav>
-            <button className="login-btn">Login</button>
-            <button className="signup-btn">Sign Up</button>
+            <button className="login-btn" onClick={() => navigate('/login')}>Login</button>
+            <button className="signup-btn" onClick={() => navigate('/register')}>Sign Up</button>
           </nav>
         </header>
         <main>
           <div className="call-to-action">
             <h2>Find & Share Great Recipes with Ease</h2>
-            <button className="start-cooking-btn" onClick={() => history.push('/login')}>Start Cooking!</button>
+            <button className="start-cooking-btn" onClick={() => navigate('/login')}>Start Cooking!</button>
           </div>
         </main>
       </div>
