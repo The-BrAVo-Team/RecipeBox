ed5912b6e301af104c7cc413b6c36d40b9129200
Bethvour Chike <bethvourc@gmail.com>
2024-01-29 20:50:02 -0600

    switch change

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LandingPage from './components/LandingPage';
 import LoginPage from './components/LoginPage';
 import RegisterPage from './components/RegisterPage';
@@ -9,12 +9,12 @@ class App extends Component {
   render() {
     return (
     <Router>
-      <Switch>
+      <Routes>
         <Route path="/" exact component={LandingPage} />
         <Route path="/login" component={LoginPage} />
         <Route path="/register" component={RegisterPage}/>
         {/* Other routes */}
-      </Switch>
+      </Routes>
     </Router>
     );
   }
