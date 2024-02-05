b73f4ffac578d5cd687f41891e0e792c795f95e5
Vincent Carlton <vcarlton@nevada.unr.edu>
2024-02-05 13:33:54 -0800

    Fixed app Problems and made sure .env is not synced

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import LandingPage from './components/LandingPage';
-import LoginPage from './components/LoginPage';
-import RegisterPage from './components/RegisterPage';
 
 class App extends Component {
   render() {
     return (
-    <Router>
-      <Routes>
-        <Route path="/" exact component={LandingPage} />
-        <Route path="/login" component={LoginPage} />
-        <Route path="/register" component={RegisterPage}/>
-        {/* Other routes */}
-      </Routes>
-    </Router>
+      <App></App>
     );
   }
 }
