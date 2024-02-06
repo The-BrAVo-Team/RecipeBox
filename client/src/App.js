e67afcb1fcad759a8ab94f4a64e1526e55a34478
agm3604 <agm3604@gmail.com>
2024-02-05 23:03:17 -0600

    Fixed navbar error on Landing Page

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+//import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 class App extends Component {
   render() {
